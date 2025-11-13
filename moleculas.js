f914d9761625d236747926dc99b46f1cb9b61bbb
Rochi <163904427+RochiC@users.noreply.github.com>
2025-11-12 23:42:58 -0300

    post

--- moleculas.js
+++ moleculas.js
@@ -72,7 +72,13 @@ async function generateMolecule(inputSmiles) {
   try {
     // ... igual que antes
     const response = await fetch(API_URL, {
-      // ...
+      method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    input_text: inputSmiles
+  })
     });
 
     if (!response.ok) {
